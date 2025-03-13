cbaa7d202bd5398418aaec1ebbcc490bad2a0963
RAHUL SANJAI R <93142924+RAHULSANJAI21@users.noreply.github.com>
2025-03-13 21:21:21 +0530

    Update controlling_user_led_using_user_switch.c

--- controlling_user_led_using_user_switch.c
+++ controlling_user_led_using_user_switch.c
@@ -1,19 +1,19 @@
 #include"stm32f4xx.h"
 int main()
 {
-    RCC->AHB1ENR|=0x00000005;
-    GPIOA->MODER|=0x00000400;
-    GPIOC->MODER&=~0x0C000000;
+    RCC->AHB1ENR|=0x00000005;//enable clock of the port
+    GPIOA->MODER|=0x00000400;//select on board led pin as o/p
+    GPIOC->MODER&=~0x0C000000;//select on board as input
     
     while(1)
     {
-        if(GPIOC->IDR &(1<<13))
+        if(GPIOC->IDR &(1<<13))//check if switch is pressed
         {
-            GPIOA->ODR|=0x00000020;
+            GPIOA->ODR|=0x00000020;//turn on led
         }
         else
         {
-             GPIOA->ODR&=~0x00000020;
+             GPIOA->ODR&=~0x00000020;//turn off led
         }
     }
 }
